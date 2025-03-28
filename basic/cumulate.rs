9376f223f40487b4736bdc8098f15c5127a0ca26
GaLLium-git <205247616+GaLLium-git@users.noreply.github.com>
2025-03-28 13:21:39 +0900

    Create cumulate.rs

---
+++ basic/cumulate.rs
@@ -0,0 +1,78 @@
+ //cumulate,cumlate_rev
+pub trait Cumulate<T> 
+    where
+      T:Copy,
+    {
+    fn cumulate<F>(&self, f: F) -> Vec<T>
+    where
+        F: Fn(T,T) -> T;
+     fn cumulate_rev<F>(&self, f: F) -> Vec<T>
+     where
+        F: Fn(T,T) -> T;
+    }
+impl<T> Cumulate<T> for Vec<T>
+    where
+      T:Copy,
+    {
+     fn cumulate<F>(&self, f: F) ->Vec<T>
+     where
+        F: Fn(T,T) ->T, 
+      {
+        let mut cumvec:Vec<T> = Vec::new();
+        let mut accum=self[0];
+        cumvec.push(accum);
+        for i in 1..self.len() {
+          accum=f(accum,self[i]);
+          cumvec.push(accum);
+        }
+        cumvec
+      }  
+     fn cumulate_rev<F>(&self, f: F) ->Vec<T>
+     where
+        F: Fn(T,T) ->T, 
+      {
+        let mut cumvec:Vec<T> = Vec::new();
+        let mut accum=self[self.len()-1];
+        cumvec.push(accum);
+        for i in (0..self.len()-1).rev() {
+          accum=f(accum,self[i]);
+          cumvec.push(accum);
+        }
+        cumvec
+      }  
+    }
+    
+  //cumlate_2d
+pub trait Cumulate2D<T> 
+    where
+      T:Copy,
+    {
+    fn cumulate_2d<F>(&self, f: F) -> Vec<Vec<T>>
+    where
+        F: Fn(T,T) -> T;
+    }
+impl<T> Cumulate2D<T> for Vec<Vec<T>>
+    where
+      T:Copy,
+    {
+     fn cumulate_2d<F>(&self, f: F) ->Vec<Vec<T>>
+     where
+        F: Fn(T,T) ->T, 
+      {
+        let mut cumvec:Vec<Vec<T>> = Vec::new();
+        for i in 0..self.len(){
+            cumvec.push(self[i].clone());
+        }
+        for i in 0..self.len() {
+          for j in 1..self[i].len(){
+            cumvec[i][j]=f(cumvec[i][j],cumvec[i][j-1]);
+          }
+        }
+        for i in 1..self.len() {
+          for j in 0..self[i].len(){
+            cumvec[i][j]=f(cumvec[i][j],cumvec[i-1][j]);
+          }
+        }
+        cumvec
+      }  
+    }
