364dca7dea233d342d79c64535bd29798cbdab38
GaLLium-git <205247616+GaLLium-git@users.noreply.github.com>
2025-03-31 15:38:54 +0900

    Update basic.rs

--- basic/basic.rs
+++ basic/basic.rs
@@ -43,7 +43,8 @@ pub mod basic{
        self.insert(0,vec![T::default();self[1].len()]);
      }
     }
-   
+
+  
   //bsearch
   pub trait BinarySearch<T> {
     fn bsearch<F>(&self, f: F) -> usize
@@ -55,21 +56,22 @@ pub mod basic{
      where
         F: Fn(&T) -> bool, 
       {
-        let mut left = -1i64; 
-        let mut right = self.len() as i64;
-        while right - left > 1 {
+        let mut left =0; 
+        let mut right = self.len();
+        while left!=right {
           let mid = left + (right - left) / 2;
-          if f(&self[mid as usize]) {right = mid;}
-          else {left = mid;}
+          if f(&self[mid]) {right = mid;}
+          else {left = mid+1;}
         }
-        right as usize
+        right
       }  
-   }
-   
+    }
+
+  
   //bsearch_range
   pub trait BinarySearchRange<T>
     where
-      T:From<u8>+PartialOrd
+      T:From<u8>+PartialOrd+Copy
           +std::ops::Add<Output=T>
           +std::ops::Sub<Output=T>
           +std::ops::Mul<Output=T>
@@ -80,30 +82,41 @@ pub mod basic{
         F: Fn(&T) -> bool;
     }
 
-  impl<T> BinarySearchRange<T> for std::ops::Range<T>
-  where
+  impl<S:std::ops::RangeBounds<T>,T> BinarySearchRange<T> for S
+    where
       T:From<u8>+PartialOrd+Copy
           +std::ops::Add<Output=T>
           +std::ops::Sub<Output=T>
           +std::ops::Mul<Output=T>
           +std::ops::Div<Output=T>,
-     {
-      fn bsearch_range<F>(&self, f: F) -> T
-      where
-        F: Fn(&T) -> bool, 
-      {
-        let mut left = self.start-T::from(1);
-        let mut right = self.end;
-        while right - left > T::from(1){
-          let mid = left + (right - left) / T::from(2);
-          if f(&mid) {right = mid;}
-          else {left = mid;}
+    {
+    fn bsearch_range<F>(&self, f: F) -> T
+    where
+        F: Fn(&T) -> bool,
+    {
+        let mut right = match self.end_bound() {
+            std::ops::Bound::Included(right) => *right +T::from(1),
+            std::ops::Bound::Excluded(right) => *right,
+            std::ops::Bound::Unbounded => panic!("No Bounded Range in Binary Search"),
+        };
+        let mut left = match self.start_bound() {
+            std::ops::Bound::Included(left) => *left,
+            std::ops::Bound::Excluded(left) => *left + T::from(1),
+            std::ops::Bound::Unbounded => panic!("No Bounded Range in Binary Search"),
+        };
+
+        assert!(left<=right);
+        while left != right {
+            let mid = left + (right - left) / T::from(2);
+            if f(&mid) {right= mid;} 
+            else { left= mid + T::from(1);}
         }
-        right
+        left
       }
     }
-    
-  //cumulate,cumlate_rev
+
+   
+   //cumulate,cumlate_rev
    pub trait Cumulate<T> 
     where
       T:Copy,
