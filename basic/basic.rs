0e32729d0e0ff381e89f28b791c8357cf32d9287
GaLLium-git <205247616+GaLLium-git@users.noreply.github.com>
2025-04-04 11:26:31 +0900

    Update basic.rs

--- basic/basic.rs
+++ basic/basic.rs
@@ -1,7 +1,7 @@
-pub mod basic{
-  //read! macro
-  #[macro_export]
-  macro_rules! init{
+pub mod basic {
+    //read! macro
+    #[macro_export]
+    macro_rules! init{
    (@inner $d:tt)=>{
       let input=std::io::read_to_string(std::io::stdin()).unwrap();
       let mut iter=input.split_whitespace();
@@ -16,183 +16,217 @@ pub mod basic{
     };
     ()=>{init!(@inner $)};
    }
-   
-  //to_1indexed
-  pub trait To1Index<T>
-    where
-      T:Default+Copy,
-    {
-    fn to_1index(&mut self);
-    }
-  impl<T> To1Index<T> for Vec<T>
-     where
-        T:Default+Copy,
-    {
-     fn to_1index(&mut self){
-         self.insert(0,T::default());
-     }
-    }
-  impl<T> To1Index<T> for Vec<Vec<T>>
-    where
-      T:Default+Copy,
-    {
-    fn to_1index(&mut self){
-       for i in 0..self.len(){
-         self[i].insert(0,T::default());
-       }
-       self.insert(0,vec![T::default();self[1].len()]);
-     }
-    }
 
-  
-  //bsearch
-  pub trait BinarySearch<T> {
-    fn bsearch<F>(&self, f: F) -> usize
+    //Shift vec
+    pub trait Shift<T>
     where
-        F: Fn(&T) -> bool;
-  }
-  impl<T> BinarySearch<T> for Vec<T>{
-     fn bsearch<F>(&self, f: F) -> usize
-     where
-        F: Fn(&T) -> bool, 
-      {
-        let mut left =0; 
-        let mut right = self.len();
-        while left!=right {
-          let mid = left + (right - left) / 2;
-          if f(&self[mid]) {right = mid;}
-          else {left = mid+1;}
+        T: Default + Copy,
+    {
+        fn shift(&mut self);
+        fn shifted(&mut self)->Vec<T>;
+    }
+    impl<T> Shift<T> for Vec<T>
+    where
+        T: Default + Copy,
+    {
+        fn shift(&mut self) {
+            self.insert(0, T::default());
         }
-        left
-      }  
-    }
-
-  
-  //bsearch_range
-  pub trait BinarySearchRange<T>
-    where
-      T:From<u8>+PartialOrd+Copy
-          +std::ops::Add<Output=T>
-          +std::ops::Sub<Output=T>
-          +std::ops::Mul<Output=T>
-          +std::ops::Div<Output=T>,
-    {
-    fn bsearch_range<F>(&self, f: F) -> T
-    where
-        F: Fn(&T) -> bool;
-    }
-
-  impl<S:std::ops::RangeBounds<T>,T> BinarySearchRange<T> for S
-    where
-      T:From<u8>+PartialOrd+Copy
-          +std::ops::Add<Output=T>
-          +std::ops::Sub<Output=T>
-          +std::ops::Mul<Output=T>
-          +std::ops::Div<Output=T>,
-    {
-    fn bsearch_range<F>(&self, f: F) -> T
-    where
-        F: Fn(&T) -> bool,
-    {
-        let mut right = match self.end_bound() {
-            std::ops::Bound::Included(right) => *right +T::from(1),
-            std::ops::Bound::Excluded(right) => *right,
-            std::ops::Bound::Unbounded => panic!("No Bounded Range in Binary Search"),
-        };
-        let mut left = match self.start_bound() {
-            std::ops::Bound::Included(left) => *left,
-            std::ops::Bound::Excluded(left) => *left + T::from(1),
-            std::ops::Bound::Unbounded => panic!("No Bounded Range in Binary Search"),
-        };
-
-        assert!(left<=right);
-        while left != right {
-            let mid = left + (right - left) / T::from(2);
-            if f(&mid) {right= mid;} 
-            else { left= mid + T::from(1);}
+        fn shifted(&mut self)->Vec<T>{
+            self.shift();
+            self.to_vec()
         }
-        left
-      }
-    }
-
-   
-   //cumulate,cumlate_rev
-   pub trait Cumulate<T> 
-    where
-      T:Copy,
-    {
-    fn cumulate<F>(&self, f: F) -> Vec<T>
-    where
-        F: Fn(T,T) -> T;
-     fn cumulate_rev<F>(&self, f: F) -> Vec<T>
-     where
-        F: Fn(T,T) -> T;
-    }
-  impl<T> Cumulate<T> for Vec<T>
-    where
-      T:Copy,
-    {
-     fn cumulate<F>(&self, f: F) ->Vec<T>
-     where
-        F: Fn(T,T) ->T, 
-      {
-        let mut cumvec:Vec<T> = Vec::new();
-        let mut accum=self[0];
-        cumvec.push(accum);
-        for i in 1..self.len() {
-          accum=f(accum,self[i]);
-          cumvec.push(accum);
-        }
-        cumvec
-      }  
-     fn cumulate_rev<F>(&self, f: F) ->Vec<T>
-     where
-        F: Fn(T,T) ->T, 
-      {
-        let mut cumvec:Vec<T> = Vec::new();
-        let mut accum=self[self.len()-1];
-        cumvec.push(accum);
-        for i in (0..self.len()-1).rev() {
-          accum=f(accum,self[i]);
-          cumvec.push(accum);
-        }
-        cumvec.reverse();
-        cumvec
-      }  
     }
     
-  //cumlate_2d
-  pub trait Cumulate2D<T> 
+   
+    pub trait Shift2D<T>
     where
-      T:Copy,
-    {
-    fn cumulate_2d<F>(&self, f: F) -> Vec<Vec<T>>
-    where
-        F: Fn(T,T) -> T;
+        T: Default + Copy,
+    {   
+        fn shift(&mut self);
+        fn shifted(&mut self)->Vec<Vec<T>>;
+        fn shift_2d(&mut self);
+        fn shifted_2d(&mut self)->Vec<Vec<T>>;
     }
-  impl<T> Cumulate2D<T> for Vec<Vec<T>>
+    impl<T> Shift2D<T> for Vec<Vec<T>>
     where
-      T:Copy,
+        T: Default + Copy,
     {
-     fn cumulate_2d<F>(&self, f: F) ->Vec<Vec<T>>
-     where
-        F: Fn(T,T) ->T, 
-      {
-        let mut cumvec:Vec<Vec<T>> = Vec::new();
-        for i in 0..self.len(){
-            cumvec.push(self[i].clone());
+         fn shift(&mut self) {
+            self.insert(0, vec![T::default();self[0].len()]);
         }
-        for i in 0..self.len() {
-          for j in 1..self[i].len(){
-            cumvec[i][j]=f(cumvec[i][j],cumvec[i][j-1]);
-          }
+        fn shifted(&mut self)-> Vec<Vec<T>>{
+           self.shift();
+           self.to_vec()
         }
-        for i in 1..self.len() {
-          for j in 0..self[i].len(){
-            cumvec[i][j]=f(cumvec[i][j],cumvec[i-1][j]);
-          }
+        fn shift_2d(&mut self) {
+            for i in 0..self.len() {
+                self[i].shift();
+            }
+            self.shift();
         }
-        cumvec
-      }  
+        fn shifted_2d(&mut self)->Vec<Vec<T>>{
+            self.shift_2d();
+            self.to_vec()
+        }
+    }
+
+    //bsearch
+    pub trait BinarySearch<T> {
+        fn bsearch<F>(&self, f: F) -> usize
+        where
+            F: Fn(&T) -> bool;
+    }
+    impl<T> BinarySearch<T> for Vec<T> {
+        fn bsearch<F>(&self, f: F) -> usize
+        where
+            F: Fn(&T) -> bool,
+        {
+            let mut left = 0;
+            let mut right = self.len();
+            while left != right {
+                let mid = left + (right - left) / 2;
+                if f(&self[mid]) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            left
+        }
+    }
+
+    //bsearch_range
+    pub trait BinarySearchRange<T>
+    where
+        T: From<u8>
+            + PartialOrd
+            + Copy
+            + std::ops::Add<Output = T>
+            + std::ops::Sub<Output = T>
+            + std::ops::Mul<Output = T>
+            + std::ops::Div<Output = T>,
+    {
+        fn bsearch_range<F>(&self, f: F) -> T
+        where
+            F: Fn(&T) -> bool;
+    }
+
+    impl<S: std::ops::RangeBounds<T>, T> BinarySearchRange<T> for S
+    where
+        T: From<u8>
+            + PartialOrd
+            + Copy
+            + std::ops::Add<Output = T>
+            + std::ops::Sub<Output = T>
+            + std::ops::Mul<Output = T>
+            + std::ops::Div<Output = T>,
+    {
+        fn bsearch_range<F>(&self, f: F) -> T
+        where
+            F: Fn(&T) -> bool,
+        {
+            let mut right = match self.end_bound() {
+                std::ops::Bound::Included(right) => *right + T::from(1),
+                std::ops::Bound::Excluded(right) => *right,
+                std::ops::Bound::Unbounded => panic!("No Bounded Range in Binary Search"),
+            };
+            let mut left = match self.start_bound() {
+                std::ops::Bound::Included(left) => *left,
+                std::ops::Bound::Excluded(left) => *left + T::from(1),
+                std::ops::Bound::Unbounded => panic!("No Bounded Range in Binary Search"),
+            };
+
+            assert!(left <= right);
+            while left != right {
+                let mid = left + (right - left) / T::from(2);
+                if f(&mid) {
+                    right = mid;
+                } else {
+                    left = mid + T::from(1);
+                }
+            }
+            left
+        }
+    }
+
+    // cumulate,cumlate_rev
+    pub trait Cumulate<T>
+    where
+        T: Copy,
+    {
+        fn cumulate<F>(&self, f: F) -> Vec<T>
+        where
+            F: Fn(T, T) -> T;
+        fn cumulate_rev<F>(&self, f: F) -> Vec<T>
+        where
+            F: Fn(T, T) -> T;
+    }
+    impl<T> Cumulate<T> for Vec<T>
+    where
+        T: Copy,
+    {
+        fn cumulate<F>(&self, f: F) -> Vec<T>
+        where
+            F: Fn(T, T) -> T,
+        {
+            let mut cumvec: Vec<T> = Vec::new();
+            let mut accum = self[0];
+            cumvec.push(accum);
+            for i in 1..self.len() {
+                accum = f(accum, self[i]);
+                cumvec.push(accum);
+            }
+            cumvec
+        }
+        fn cumulate_rev<F>(&self, f: F) -> Vec<T>
+        where
+            F: Fn(T, T) -> T,
+        {
+            let mut cumvec: Vec<T> = Vec::new();
+            let mut accum = self[self.len() - 1];
+            cumvec.push(accum);
+            for i in (0..self.len() - 1).rev() {
+                accum = f(accum, self[i]);
+                cumvec.push(accum);
+            }
+            cumvec.reverse();
+            cumvec
+        }
+    }
+
+    //cumlate_2d
+    pub trait Cumulate2D<T>
+    where
+        T: Copy,
+    {
+        fn cumulate_2d<F>(&self, f: F) -> Vec<Vec<T>>
+        where
+            F: Fn(T, T) -> T;
+    }
+    impl<T> Cumulate2D<T> for Vec<Vec<T>>
+    where
+        T: Copy,
+    {
+        fn cumulate_2d<F>(&self, f: F) -> Vec<Vec<T>>
+        where
+            F: Fn(T, T) -> T,
+        {
+            let mut cumvec: Vec<Vec<T>> = Vec::new();
+            for i in 0..self.len() {
+                cumvec.push(self[i].clone());
+            }
+            for i in 0..self.len() {
+                for j in 1..self[i].len() {
+                    cumvec[i][j] = f(cumvec[i][j], cumvec[i][j - 1]);
+                }
+            }
+            for i in 1..self.len() {
+                for j in 0..self[i].len() {
+                    cumvec[i][j] = f(cumvec[i][j], cumvec[i - 1][j]);
+                }
+            }
+            cumvec
+        }
     }
 }
