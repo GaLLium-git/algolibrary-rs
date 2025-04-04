0d695f37b0517cfe4f2e1765a845d8dbe7baf082
GaLLium-git <205247616+GaLLium-git@users.noreply.github.com>
2025-04-04 20:29:06 +0900

    Create shift.rs

---
+++ algo/shift.rs
@@ -0,0 +1,37 @@
+pub trait Shift<T>
+    where
+        T: Default + Copy,
+    {
+        fn shift(&mut self);
+    }
+    impl<T> Shift<T> for Vec<T>
+    where
+        T: Default + Copy,
+    {
+        fn shift(&mut self) {
+            self.insert(0, T::default());
+        }
+    }
+    
+   
+pub trait Shift2D<T>
+    where
+        T: Default + Copy,
+    {   
+        fn shift(&mut self);
+        fn shift_2d(&mut self);
+    }
+    impl<T> Shift2D<T> for Vec<Vec<T>>
+    where
+        T: Default + Copy,
+    {
+         fn shift(&mut self) {
+            self.insert(0, vec![T::default();self[0].len()]);
+        }
+        fn shift_2d(&mut self) {
+            for i in 0..self.len() {
+                self[i].shift();
+            }
+            self.shift();
+        }
+    }
