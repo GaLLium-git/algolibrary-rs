6a52e39071e64768347f65f00a9a1a981b1e6d11
GaLLium-git <205247616+GaLLium-git@users.noreply.github.com>
2025-04-04 20:10:21 +0900

    Create subsetsum.rs

---
+++ dp/subsetsum.rs
@@ -0,0 +1,18 @@
+fn main() {
+　　init!();
+    let (n, s) = read!(usize, usize);
+    let a = read!(usize;n).shifted();
+    let mut dp=vec![vec![false;s+1];n+1];
+    dp[0][0]=true;
+    for i in 1..=n{
+        for j in 0..=s{
+            if j < a[i]{
+                dp[i][j]=dp[i-1][j];
+            } else {
+                dp[i][j]=dp[i-1][j] || dp[i-1][j-a[i]];
+            }
+        }
+    }
+    
+    println!("{}",if dp[n][s] {"Yes"} else {"No"});
+}
