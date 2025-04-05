dfde6300fcf10044bb5af3e3bc42d18a848dface
GaLLium-git <205247616+GaLLium-git@users.noreply.github.com>
2025-04-05 15:53:44 +0900

    Create knapsack.rs

---
+++ dp/knapsack.rs
@@ -0,0 +1,23 @@
+fn main() {
+    init!();
+    let (n, weigh) = read!(usize, usize);
+    let mut w=vec![0;n+1];
+    let mut v=vec![0;n+1];
+    for i in 1..=n{
+        (w[i],v[i])=read!(usize,usize);
+    }
+    
+    let mut dp=vec![vec![0usize;weigh+1];n+1];
+   
+    for i in 1..=n{
+        for j in 0..=weigh{
+            if j < w[i]{
+                dp[i][j]=dp[i-1][j];
+            } else {
+                dp[i][j]=std::cmp::max(dp[i-1][j],dp[i-1][j-w[i]]+v[i]);
+            }
+        }
+    }
+    //println!("{:?}",dp[n]);
+    println!("{}",dp[n].iter().max().unwrap());
+}
