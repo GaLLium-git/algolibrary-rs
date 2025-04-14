168446e91f5bb2d9ff2c32cf15025515a003bbf2
GaLLium-git <205247616+GaLLium-git@users.noreply.github.com>
2025-04-14 10:19:29 +0900

    Update numbertheory.rs

--- math/numbertheory.rs
+++ math/numbertheory.rs
@@ -1,5 +1,8 @@
-pub mod numbertheory{
-  pub fn digits(mut n:usize,base:usize)->Vec<usize>{
+fn main(){
+    \\nothing here..
+}
+
+pub fn digits(mut n:usize,base:usize)->Vec<usize>{
     let mut v=Vec::new();
     while n>0{
         v.push(n%base);
@@ -7,17 +10,17 @@ pub mod numbertheory{
      }
     v.reverse();
     v
-   }
+}
  
-  pub fn gcd(n:usize,m:usize)->usize{
+pub fn gcd(n:usize,m:usize)->usize{
     if m==0 {
         n
     } else{
         gcd(m,n%m)
     }
-  }
+}
 
-  pub fn is_prime(n:usize)->bool{
+pub fn is_prime(n:usize)->bool{
     let mut is_prime=true;
     for i in 2..=num::integer::sqrt(n){  //can use usize::isqrt in std from Rust 1.84
         if n%i==0{
@@ -25,5 +28,5 @@ pub mod numbertheory{
         }
     }
     is_prime
-  }
 }
+
