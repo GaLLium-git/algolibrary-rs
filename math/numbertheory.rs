43d3b734bca68ed668d29182920307d26ab5da43
GaLLium-git <205247616+GaLLium-git@users.noreply.github.com>
2025-04-14 09:29:01 +0900

    Update numbertheory.rs

--- math/numbertheory.rs
+++ math/numbertheory.rs
@@ -8,11 +8,22 @@ pub mod numbertheory{
     v.reverse();
     v
    }
+ 
   pub fn gcd(n:usize,m:usize)->usize{
     if m==0 {
         n
     } else{
         gcd(m,n%m)
     }
+  }
+
+  pub fn is_prime(n:usize)->bool{
+    let mut is_prime=true;
+    for i in 2..=num::integer::sqrt(n){  //can use usize::isqrt in std from Rust 1.84
+        if n%i==0{
+            is_prime=false;
+        }
+    }
+    is_prime
   }
 }
