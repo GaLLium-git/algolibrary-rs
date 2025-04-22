2453888c2abe4f5129f2e44f948249ca74f284f7
GaLLium-git <205247616+GaLLium-git@users.noreply.github.com>
2025-04-22 19:00:09 +0900

    Update knapsack.rs

--- dp/knapsack.rs
+++ dp/knapsack.rs
@@ -14,7 +14,7 @@ fn main() {
             if j < w[i]{
                 dp[i][j]=dp[i-1][j];
             } else {
-                dp[i][j]=std::cmp::max(dp[i-1][j],dp[i-1][j-w[i]]+v[i]);
+                dp[i][j]=dp[i-1][j].max(dp[i-1][j-w[i]]+v[i]);
             }
         }
     }
