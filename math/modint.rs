a93b27165e07d385c6403499588e3b8984f341d0
GaLLium-git <205247616+GaLLium-git@users.noreply.github.com>
2025-04-23 05:12:15 +0900

    Update modint.rs

--- math/modint.rs
+++ math/modint.rs
@@ -1,6 +1,6 @@
 #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct ModInt {
-    value: usize,
+    val: usize,
 }
 
 pub static MOD: std::sync::OnceLock<usize> = std::sync::OnceLock::new();
@@ -18,17 +18,17 @@ impl ModInt {
     pub fn new(value: usize) -> Self {
         let modulus = Self::modulus();
         ModInt {
-            value: value % modulus,
+            val: value % modulus,
         }
     }
 
     pub fn inv(self) -> Self {
-        self.pow(Self::modulus()-2)
+        self.pow(Self::modulus() - 2)
     }
 
     pub fn pow(self, exp: usize) -> Self {
         let modulus = Self::modulus();
-        let mut base = self.value;
+        let mut base = self.val;
         let mut result = 1;
         let mut exp = exp;
 
@@ -42,16 +42,13 @@ impl ModInt {
 
         ModInt::new(result)
     }
-
-    
 }
 
 impl std::ops::Add for ModInt {
     type Output = Self;
 
     fn add(self, rhs: Self) -> Self {
-        let modulus = Self::modulus();
-        ModInt::new(self.value + rhs.value)
+        ModInt::new(self.val + rhs.val)
     }
 }
 
@@ -60,7 +57,7 @@ impl std::ops::Sub for ModInt {
 
     fn sub(self, rhs: Self) -> Self {
         let modulus = Self::modulus();
-        ModInt::new(self.value + modulus - rhs.value)
+        ModInt::new(self.val + modulus - rhs.val)
     }
 }
 
@@ -68,8 +65,7 @@ impl std::ops::Mul for ModInt {
     type Output = Self;
 
     fn mul(self, rhs: Self) -> Self {
-        let modulus = Self::modulus();
-        ModInt::new(self.value * rhs.value)
+        ModInt::new(self.val * rhs.val)
     }
 }
 
@@ -77,36 +73,54 @@ impl std::ops::Div for ModInt {
     type Output = Self;
 
     fn div(self, rhs: Self) -> Self {
-        let modulus = Self::modulus();
-        ModInt::new(self.value * rhs.inv().value)
+        ModInt::new(self.val * rhs.inv().val)
     }
 }
 
 impl std::ops::AddAssign for ModInt {
     fn add_assign(&mut self, rhs: Self) {
         let modulus = Self::modulus();
-        self.value = (self.value + rhs.value) % modulus;
+        self.val = (self.val + rhs.val) % modulus;
     }
 }
 
 impl std::ops::SubAssign for ModInt {
     fn sub_assign(&mut self, rhs: Self) {
         let modulus = Self::modulus();
-        self.value = (self.value + modulus - rhs.value) % modulus;
+        self.val = (self.val + modulus - rhs.val) % modulus;
     }
 }
 
 impl std::ops::MulAssign for ModInt {
     fn mul_assign(&mut self, rhs: Self) {
         let modulus = Self::modulus();
-        self.value = (self.value * rhs.value) % modulus;
+        self.val = (self.val * rhs.val) % modulus;
     }
 }
 
 impl std::ops::DivAssign for ModInt {
     fn div_assign(&mut self, rhs: Self) {
         let modulus = Self::modulus();
-        self.value = (self.value * rhs.inv().value) % modulus;
+        self.val = (self.val * rhs.inv().val) % modulus;
     }
 }
+
+impl std::fmt::Display for ModInt {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        std::write!(f, "{}", self.val)
+    }
+}
+
+impl std::str::FromStr for ModInt {
+    type Err = std::string::String;
+
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        let parsed_val = s
+            .trim()
+            .parse::<usize>()
+            .map_err(|e| std::format!("Failed to parse ModInt from string: {}", e))?;
+        std::result::Result::Ok(ModInt::new(parsed_val))
+    }
+}
+
 
