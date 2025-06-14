d9edd1674211bd8e0b0102c35d877ba73ef2d4e3
GaLLium-git <205247616+GaLLium-git@users.noreply.github.com>
2025-06-14 15:13:53 +0900

    Create convolution_or.rs

---
+++ math/convolution_or.rs
@@ -0,0 +1,56 @@
+fn main() {
+    let a = vec![1, 2, 3, 4];
+    let b = vec![5, 6, 7, 8];
+    let c = convolution_or(&a, &b);
+    println!("{:?}", c);
+}
+
+
+
+pub fn zeta_transform_or<T>(f: &mut [T])
+where
+    T: Copy + std::ops::AddAssign,
+{
+    let n = f.len().trailing_zeros() as usize;
+    for i in 0..n {
+        for mask in 0..f.len() {
+            if (mask & (1 << i)) != 0 {
+                f[mask] += f[mask ^ (1 << i)];
+            }
+        }
+    }
+}
+
+pub fn mobius_transform_or<T>(f: &mut [T])
+where
+    T: Copy + std::ops::SubAssign,
+{
+    let n = f.len().trailing_zeros() as usize;
+    for i in 0..n {
+        for mask in 0..f.len() {
+            if (mask & (1 << i)) != 0 {
+                f[mask] -= f[mask ^ (1 << i)];
+            }
+        }
+    }
+}
+
+pub fn convolution_or<T>(a: &[T], b: &[T]) -> Vec<T>
+where
+    T: Copy
+        + Default
+        + std::ops::AddAssign
+        + std::ops::SubAssign
+        + std::ops::Mul<Output = T>,
+{
+    assert_eq!(a.len(), b.len());
+    let mut fa = a.to_vec();
+    let mut fb = b.to_vec();
+    zeta_transform_or(&mut fa);
+    zeta_transform_or(&mut fb);
+    for i in 0..fa.len() {
+        fa[i] = fa[i] * fb[i];
+    }
+    mobius_transform_or(&mut fa);
+    fa
+}
